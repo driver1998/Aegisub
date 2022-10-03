1848fdfa492ccc5224296ecce1a9aa980a921d35
GH Cao <driver1998.ms@outlook.com>
2022-10-03 14:13:36 +0800

    Use original Lua instead of LuaJIT
    LuaJIT does not support Windows ARM64 yet, use original
    non-JIT Lua instead.

--- vendor/luabins/src/luaheaders.h
+++ vendor/luabins/src/luaheaders.h
@@ -7,6 +7,26 @@ extern "C" {
 
 #include <lua.h>
 #include <lauxlib.h>
+
+#if (LUA_VERSION_NUM == 502)
+
+#undef lua_equal
+#define lua_equal(L,idx1,idx2)  lua_compare(L,(idx1),(idx2),LUA_OPEQ)
+
+#undef lua_getfenv
+#define lua_getfenv	lua_getuservalue
+#undef lua_setfenv
+#define lua_setfenv	lua_setuservalue
+
+#undef lua_objlen
+#define lua_objlen	lua_rawlen
+
+#undef luaL_register
+#define luaL_register(L,n,f) \
+	{ if ((n) == NULL) luaL_setfuncs(L,f,0); else luaL_newlib(L,f); }
+
+#endif
+
 #if defined (__cplusplus) && !defined (LUABINS_LUABUILTASCPP)
 }
 #endif
